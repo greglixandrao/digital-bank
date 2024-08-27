1e837e675ee693d699601378352d9a6daab354f9
Greg Lixandrão <contato@greglixandrao.com>
2024-08-27 18:46:06 -0300

    test: add money to CurrentAccount and transfer to SavingsAccount

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,19 @@ public class Main {
     public static void main(String[] args) {
 
         Account account01 = new CurrentAccount();
+        account01.deposit(100);
         Account account02 = new SavingsAccount();
         Account account03 = new CurrentAccount();
 
         account01.printStatement();
         account02.printStatement();
         account03.printStatement();
+        System.out.println("-------------------------------------------------");
+        System.out.println("-------------------------------------------------");
+
+        account01.transfer(100, account02);
+        account01.printStatement();
+        account02.printStatement();
 
     }
 }
