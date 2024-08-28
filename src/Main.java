335b085bc7292397af8f00009ea210bf24f886ab
Greg Lixandrão <contato@greglixandrao.com>
2024-08-28 18:32:36 -0300

    feat: modified Main file

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        Account account01 = new CurrentAccount();
+        System.out.println("===== Bem Vindo ao Banco Digital da Claro =====");
+        System.out.println("Escolha uma das opções abaixo:");
+        System.out.println("1 - Abrir Conta Corrente");
+        System.out.println("2 - Abrir Conta Poupança");
+        System.out.println("3 - Depositar em Conta Corrente");
+        System.out.println("4 - Depositar em Conta Poupança");
+
+
+        /*Client client01 = new Client();
+        client01.setName("John Doe");
+
+        Account account01 = new CurrentAccount(client01);
         account01.deposit(100);
-        Account account02 = new SavingsAccount();
-        Account account03 = new CurrentAccount();
+        Account account02 = new SavingsAccount(client01);
+        Account account03 = new CurrentAccount(client01);
 
         account01.printStatement();
         account02.printStatement();
@@ -16,7 +30,7 @@ public class Main {
 
         account01.transfer(100, account02);
         account01.printStatement();
-        account02.printStatement();
-
+        account02.printStatement();*/
+        scanner.close();
     }
 }
